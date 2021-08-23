dd7b5376e34b418170ce41d4d6f0949b72d6beca
Genceliyev Nicat <nicatgenceliyev1122@gmail.com>
2021-08-23 11:36:25 +0400

    sacRengi degiskeni eklendi

--- src/Ogrenci/Ogrenci.java
+++ src/Ogrenci/Ogrenci.java
@@ -1,13 +1,13 @@
 package Ogrenci;
 
 public class Ogrenci {
-	private String ad, soyad, okuduguSinif, okuduguOkul, okuduguBolge;
+	private String ad, soyad, okuduguSinif, okuduguOkul, okuduguBolge, sacRengi;
 
 	private int yas;
 	private double boy;
 	private char cinsiyet;
 	
-	Ogrenci(String ad, String soyad, String okduguSinif, String okuduguOkul, String okuduguBolge, int yas, double boy, char cinsiyet) {
+	Ogrenci(String ad, String soyad, String okduguSinif, String okuduguOkul, String okuduguBolge, int yas, double boy, char cinsiyet, String sacRengi) {
 		this.ad = ad;
 		this.soyad = soyad;
 		this.okuduguSinif = okuduguSinif;
@@ -16,11 +16,12 @@ public class Ogrenci {
 		this.yas = yas;
 		this.boy = boy;
 		this.cinsiyet = cinsiyet;
+		this.sacRengi = sacRengi;
 	}
 	
 	public static void main(String[] args) {
-		Ogrenci ogrenci1 = new Ogrenci("Nicat", "Genceliyev", "10e", "181", "Merdekan", 14, 1.68, 'k');
-		Ogrenci ogrenci2 = new Ogrenci("Murad", "Mammedov", "10e", "181", "Merdekan", 15, 1.71, 'k');
-		Ogrenci ogrenci3 = new Ogrenci("Ayxan", "Naqdaliyev", "10e", "181", "Merdekan", 14, 1.66, 'k');
+		Ogrenci ogrenci1 = new Ogrenci("Nicat", "Genceliyev", "10e", "181", "Merdekan", 14, 1.68, 'k', "Siyah");
+		Ogrenci ogrenci2 = new Ogrenci("Murad", "Mammedov", "10e", "181", "Merdekan", 15, 1.71, 'k', "Siyah");
+		Ogrenci ogrenci3 = new Ogrenci("Ayxan", "Naqdaliyev", "10e", "181", "Merdekan", 14, 1.66, 'k', "Siyah");
 	}
 }
